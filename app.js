43d3446fff8768e09be688703b5e167d08aca6f8
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-03-03 03:07:19 -0500

    adjusted styles for settings

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ $(document).ready(function () {
     event.preventDefault();
     $(this).find('.btn').toggleClass('active');
 
-    // if ($(this).find('.btn-primary').length > 0) {
-    //   $(this).find('.btn').toggleClass('btn-primary');
-    // }
+    if ($(this).find('.btn-primary').length > 0) {
+      $(this).find('.btn').toggleClass('btn-primary');
+    }
     if ($(this).find('.btn-danger').length > 0) {
       $(this).find('.btn').toggleClass('btn-danger');
     }
@@ -16,17 +16,13 @@ $(document).ready(function () {
       $(this).find('.btn').toggleClass('btn-info');
     }
 
-    // $(this).find('.btn').toggleClass('btn-default');
+    $(this).find('.btn').toggleClass('btn-default');
 
 
   });
   $(document).on('click', '#more-settings-btn', function (event) {
     event.preventDefault();
     console.log("settings");
-    // your code to handle the button click goes here
-  });
-  $(document).on('click', '#more-settings-btn', function (event) {
-    event.preventDefault();
     // your code to handle the button click goes here
   });
 });
