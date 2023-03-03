01a58f0ea2f4363e480b3e48395d47799e7138dc
delacruzralph <ralphlewisdelacruz@gmail.com>
2023-03-03 02:26:33 -0500

    html, node, npm, jquery, bootstrap settings

---
+++ app.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(document).on('click', '.btn-toggle', function (event) {
+    event.preventDefault();
+    $(this).find('.btn').toggleClass('active');
+
+    // if ($(this).find('.btn-primary').length > 0) {
+    //   $(this).find('.btn').toggleClass('btn-primary');
+    // }
+    if ($(this).find('.btn-danger').length > 0) {
+      $(this).find('.btn').toggleClass('btn-danger');
+    }
+    if ($(this).find('.btn-success').length > 0) {
+      $(this).find('.btn').toggleClass('btn-success');
+    }
+    if ($(this).find('.btn-info').length > 0) {
+      $(this).find('.btn').toggleClass('btn-info');
+    }
+
+    // $(this).find('.btn').toggleClass('btn-default');
+
+
+  });
+  $(document).on('click', '#more-settings-btn', function (event) {
+    event.preventDefault();
+    console.log("settings");
+    // your code to handle the button click goes here
+  });
+  $(document).on('click', '#more-settings-btn', function (event) {
+    event.preventDefault();
+    // your code to handle the button click goes here
+  });
+});
